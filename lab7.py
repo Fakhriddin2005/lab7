55814555db1b990c24cb46953a3414ffc1c06e9d
Fakhriddin2005 <fakhriddinabdurahimov8@gmail.com>
2024-04-11 00:03:58 +0300

    Initial commit

---
+++ lab7.py
@@ -0,0 +1,39 @@
+# def t1():
+#  from PIL import Image
+#  a = "starwars.jpg"
+#  with Image.open(a) as img:
+#   img.load()
+#   img.show()
+#   w, h = img.size
+#   format = img.format
+#   mode = img.mode
+#   print('Ширина: ',w)
+#   print('Высота: ',h)
+#   print('Формат: ', format)
+#   print('Цвет: ',mode)
+# t1()
+
+# def t2():
+#  from PIL import Image
+#  b="starwars.jpg"
+#  with Image.open(b) as img:
+#   img.load()
+#
+#   img2 = img.resize((img.width //3, img.height //3))
+#   img2.save('starwars_resize.jpg')
+#
+#   img3 = img2.rotate(90)
+#   img3.save("starwars_rot.jpg")
+#
+#   img4 = img.transpose(img2.FLIP_LEFT_RIGHT)
+#   img4.save("starwar_mir.jpg")
+# t2()
+
+def t3():
+ from PIL import Image
+ c = "1.jpg"
+ with Image.open(c) as img:
+  img.load()
+  img.show()
+
+ t3()
